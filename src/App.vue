3a47a4ca392188d0f6261fa65f433ef114c7d4dc
Борислав <borislavizmestiev@gmail.com>
2026-03-13 21:42:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainContentWrapper from "./components/MainContentWrapper.vue";
+import Button from "./components/Button.vue";
+</script>
+
+<template>
+  <MainContentWrapper>
+    <Button>Начать игру</Button>
+  </MainContentWrapper>
+</template>
+
+<style scoped></style>
---
+++ src/components/MainContentWrapper.vue
@@ -0,0 +1,25 @@
+<script></script>
+
+<template>
+  <div class="main_content_wrapper">
+    <div class="main_content_box">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main_content_wrapper {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  place-items: center;
+}
+
+.main_content_box {
+  margin: 0 auto;
+  width: 80%;
+  height: 500px;
+  border: 1px solid #ffffff;
+}
+</style>
